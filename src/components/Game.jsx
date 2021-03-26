84fe681a0295866fa054b56a09cee93446bbf213
Joseph Richardson <richardsonjp93@gmail.com>
2021-03-26 14:05:53 +0000

    handleClick now puts an X or O on the board if there is not already a winner or the square is not occupied

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,11 +5,18 @@ import {calculateWinner} from '../helpers';
 const Game = () => {
 
   const [board, setBoard] = useState(Array(9).fill(null));
-  const [xIsNext, setxIsNext] = useState(true);
+  const [xIsNext, setXIsNext] = useState(true);
   const winner = calculateWinner(board);
 
-  const handleClick = () => {
+  const handleClick = (i) => {
+    const boardCopy = [...board];
 
+    if(winner || boardCopy[i]) return;
+
+    boardCopy[i] = xIsNext ? 'X' : 'O';
+    
+    setBoard(boardCopy);
+    setXIsNext(!xIsNext);
   }
 
   const jumpTo = () => {
